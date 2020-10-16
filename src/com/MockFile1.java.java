a1ee2979b89b97ae0c9c7a45b54339fc95959251
Shan <Shanmugasundaram.palanisamy@microfocus.com>
2020-10-16 16:35:55 +0530

    Adding some changes now

---
+++ src/com/MockFile1.java.java
@@ -0,0 +1,16 @@
+package shan.examples.ds.algo.impl;
+
+public class TestFile1 {
+
+	int arr[];
+	int size;
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("For testing purpose");
+		
+	}
+	
+	
+}
